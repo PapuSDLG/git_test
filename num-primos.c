27ad87c44e5ab16f6d367b2b7c908c814314c1fd
Kevin <droidekyp956@gmail.com>
2024-05-22 20:42:44 -0300

    numeros primos en C

--- num-primos.c
+++ num-primos.c
@@ -5,10 +5,8 @@ int main(int argc, char* argv[]) {
   int num = atoi(argv[1]);
   int contador = 0;
   for (int i = 1; i <= num; i++) {
-    if (num % i > 0) {
-    }
-    else {
-      contador += 1;
+    if (num % i == 0) {
+	contador += 1;
     }
   }
   if (contador == 2) {
